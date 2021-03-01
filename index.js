4887a82ab8659c7c24e166bdbca3cb8836dcba79
Gage Eide <gageeide@gmail.com>
2021-03-01 15:26:59 -0600

    Got 'me' request working

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const fetch = require('node-fetch');
 
 const email = process.env.email;
 const password = process.env.password;
-let authToken = '';
+let authToken;
+let courseId;
 
 (async () => {
     try {
+        // Login request
         let loginRes = await fetch("https://bootcampspot.com/api/instructor/v1/login", {
             method: 'post',
             body: JSON.stringify({email, password})
@@ -19,29 +21,48 @@ let authToken = '';
         }
 
         loginRes = await loginRes.json();
+        // Set authToken
         authToken = loginRes.authenticationInfo.authToken;
 
-        let attendanceRes = await fetch("https://bootcampspot.com/api/instructor/v1/attendance", {
-            method: 'post',
+        // Me request to get courseId from enrollments array in response
+        let meRes = await fetch("https://bootcampspot.com/api/instructor/v1/me", {
             headers: {
                 'Content-Type': 'application/json',
                 'authToken': authToken
-            },
-            body: JSON.stringify({"courseId": 3296})
-        })
-
-        if (attendanceRes.status >= 400) {
-            throw new Error("Bad response from login request");
-        }
-
-        attendanceRes = await attendanceRes.json();
-        fs.writeFile('./data/attendance.json', JSON.stringify(attendanceRes), 'utf-8', (err) => {
-            if (err) {
-                return console.log(err);
             }
-            
-            console.log("Done! Attendance saved to data/attendance.json");
         });
+
+        if (meRes.status >= 400) {
+            console.log(meRes)
+            throw new Error("Bad response from me request");
+        }
+        
+        meRes = await meRes.json();
+        courseId = meRes.Enrollments[0].courseId;
+        console.log(courseId);
+
+        // let attendanceRes = await fetch("https://bootcampspot.com/api/instructor/v1/attendance", {
+        //     method: 'post',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         'authToken': authToken
+        //     },
+        //     body: JSON.stringify({"courseId": 3296})
+        // })
+
+        // if (attendanceRes.status >= 400) {
+        //     throw new Error("Bad response from login request");
+        // }
+
+        // attendanceRes = await attendanceRes.json();
+        
+        // fs.writeFile('./data/attendance.json', JSON.stringify(attendanceRes), 'utf-8', (err) => {
+        //     if (err) {
+        //         return console.log(err);
+        //     }
+
+        //     console.log("Done! Attendance saved to data/attendance.json");
+        // });
         
     } catch(err) {
         console.log(err)
