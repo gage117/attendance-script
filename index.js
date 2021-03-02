7d301a610550e087c3bda6e5c116ac4f7a7c4894
Gage Eide <gageeide@gmail.com>
2021-03-02 11:05:49 -0600

    Finally realized the API isn't set up in a way that I can properly retrieve info. Shelving script and will continue if the API adds an ability to grab session attendance by sessionId

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ const email = process.env.email;
 const password = process.env.password;
 let authToken;
 let courseId;
+let enrollmentId;
+const today = new Date().getDay();
+
+/**
+ * I swear if they didn't make me do so many fetch requests to get the appropriate data I need I wouldn't be making this many fetch requests.
+ * 
+ * Why I need these endpoints:
+ * login - returns authToken needed for other routes
+ * me - returns array of Enrollments that is used to grab the courseId for the attendance request
+ * attendance - returns an array of attendance for ALL sessions
+ * sessions - returns an array of sessions to compare with today's date to get correct session for today
+ * 
+ * 
+ * Once we have the array of attendance
+ */
 
 (async () => {
     try {
@@ -38,24 +53,45 @@ let courseId;
         }
         
         meRes = await meRes.json();
+        // Get courseId from first index in Enrollments array
         courseId = meRes.Enrollments[0].courseId;
-        console.log(courseId);
+        enrollmentId = meRes.Enrollments[0].id;
+        // Get attendance array
+        let attendanceRes = await fetch("https://bootcampspot.com/api/instructor/v1/attendance", {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+                'authToken': authToken
+            },
+            body: JSON.stringify({courseId})
+        });
 
-        // let attendanceRes = await fetch("https://bootcampspot.com/api/instructor/v1/attendance", {
-        //     method: 'post',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //         'authToken': authToken
-        //     },
-        //     body: JSON.stringify({"courseId": 3296})
-        // })
+        if (attendanceRes.status >= 400) {
+            throw new Error("Bad response from attendance request");
+        }
 
-        // if (attendanceRes.status >= 400) {
-        //     throw new Error("Bad response from login request");
-        // }
-
-        // attendanceRes = await attendanceRes.json();
+        attendanceRes = await attendanceRes.json();
         
+        let sessionRes = await fetch("https://bootcampspot.com/api/instructor/v1/sessions", {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+                'authToken': authToken
+            },
+            body: JSON.stringify({enrollmentId})
+        });
+
+        if (sessionRes.status >= 400) {
+            throw new Error("Bad response from session request");
+        }
+
+        sessionRes = await sessionRes.json();
+        const testDate = new Date(sessionRes.currentWeekSessions[0].session.startTime)
+        // console.log(sessionRes.currentWeekSessions.length);
+        // console.log(today)
+        // console.log(sessionRes.currentWeekSessions[today - 1]);
+        console.log(attendanceRes)
+
         // fs.writeFile('./data/attendance.json', JSON.stringify(attendanceRes), 'utf-8', (err) => {
         //     if (err) {
         //         return console.log(err);
